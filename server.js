8599d0819518f508105d98600c25a1a596373901
Rodrigo Bernardino <rodgober7@hotmail.com>
2024-11-11 16:43:08 -0600

    Versión 0.91

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/perfil', perfilRoutes);
 app.use('/api/reto', retoRoutes);
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGO_URI_Local, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('No se pudo conectar a MongoDB', err));
 
